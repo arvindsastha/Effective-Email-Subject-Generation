09dc0776ad49a705bca2effbfc0a2e9b6b184ca3
Arvind Sastha <arvindsastha@gmail.com>
2019-11-01 15:31:05 -0400

    Added email parsing util

--- Email_Preprocessing.py
+++ Email_Preprocessing.py
@@ -4,10 +4,25 @@ Created on Fri Oct 20 14:57:33 2019
 """
 
 from os import listdir
+from os.path import join as os_join
+from email.parser import Parser
 
-email_file_dir = '..\\Dataset\\maildir\\allen-p\\inbox\\'
-print(listdir(email_file_dir))
+employee_email_folder = '..\\Dataset\\maildir\\allen-p\\inbox\\'
+print(listdir(employee_email_folder))
 
-#def identify_enitites(arguments): 
-  #receive an argument of defined type
-  #return the processed value
+def parse_emails_in_folder(folder_path):
+  
+  parsed_emails_list = []
+  parser = Parser()
+  
+  for email_file_name in listdir(folder_path):
+    
+    filename = os_join(folder_path, email_file_name)
+    with open(filename, "r") as raw_email_file:
+      raw_email_text = raw_email_file.read()
+    
+    parsed_email = parser.parsestr(raw_email_text)
+    parsed_emails_list.append(parsed_email)
+  
+  return parsed_emails_list
+  
