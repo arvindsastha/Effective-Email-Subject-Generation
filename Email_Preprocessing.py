44af0aa00f3b5171673cfc6dcf41d4c415877cc5
Arvind Sastha <arvindsastha@gmail.com>
2019-11-01 15:03:58 -0400

    Created email preprocessing file

---
+++ Email_Preprocessing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 14:57:33 2019
+"""
+
+from os import listdir
+
+email_file_dir = '..\\Dataset\\maildir\\allen-p\\inbox\\'
+print(listdir(email_file_dir))
+
+#def identify_enitites(arguments): 
+  #receive an argument of defined type
+  #return the processed value
